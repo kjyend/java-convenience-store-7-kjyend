4668a4ed2cd6225a14807f355be31a634190ec7e
dbdb456 <adad568@naver.com>
2024-11-06 23:11:38 +0900

    feat(outputview): 재품 항목 출력 메서드 추가

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,9 +1,20 @@
 package store.view;
 
+import java.util.List;
+import store.domain.Product;
+
 public class OutputView {
 
     public void startGame() {
         System.out.println("안녕하세요. W편의점입니다.");
         System.out.println("현재 보유하고 있는 상품입니다.\n");
+    }
+
+    public void printProductList(List<Product> products) {
+        for (Product product : products) {
+            System.out.println(
+                    "- " + product.getName() + " " + product.getPriceUtil() + "원 " + product.getNumbers() + "개 "
+                            + product.getPromotion);
+        }
     }
 }
