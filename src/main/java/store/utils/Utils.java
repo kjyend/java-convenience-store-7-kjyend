0d560dfd9eaf20354c19e5336f1ed8864c3f3960
dbdb456 <adad568@naver.com>
2024-11-07 21:00:34 +0900

    feat(utils): 파일 읽고 리스트 반환하는 메서드 추가

--- src/main/java/store/utils/Utils.java
+++ src/main/java/store/utils/Utils.java
@@ -1,4 +1,19 @@
 package store.utils;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Utils {
+
+    public List<String> readFile(String files) {
+        try {
+            Path path = Paths.get(files);
+            return Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("[ERROR] 경로에 파일이 존재하지 않습니다.");
+        }
+    }
 }
