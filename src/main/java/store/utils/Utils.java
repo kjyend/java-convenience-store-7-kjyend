4e583c179106d32200a32083dfba314674358ca6
dbdb456 <adad568@naver.com>
2024-11-07 21:41:36 +0900

    feat(utils): String 리스트 Product 리스트 변환 메서드 추가

--- src/main/java/store/utils/Utils.java
+++ src/main/java/store/utils/Utils.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import store.domain.Product;
+import store.domain.Promotion;
 
 public class Utils {
 
@@ -14,6 +16,14 @@ public class Utils {
             return Files.readAllLines(path);
         } catch (IOException e) {
             throw new IllegalArgumentException("[ERROR] 경로에 파일이 존재하지 않습니다.");
+        }
+    }
+
+    public void buildProductList(List<String> fileProducts, List<Product> products) {
+        for (int i = 1; i < fileProducts.size(); i++) {
+            String[] fileProduct = fileProducts.get(i).split(",");
+            products.add(new Product(fileProduct[0], Integer.parseInt(fileProduct[1]), fileProduct[2],
+                    Promotion.nameOf(fileProduct[3])));
         }
     }
 }
