a0da170507407b1237994a4d15d98ff3e94963d1
dbdb456 <adad568@naver.com>
2024-11-07 20:41:42 +0900

    feat(validators): Y, N 입력 검증 메서드 추가

--- src/main/java/store/validator/Validators.java
+++ src/main/java/store/validator/Validators.java
@@ -8,4 +8,10 @@ public class Validators {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         }
     }
+
+    public void validateYesOrNo(String yesNoInput) {
+        if (!(yesNoInput.equalsIgnoreCase("y") || yesNoInput.equalsIgnoreCase("n"))) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 입력입니다. 다시 입력해 주세요.");
+        }
+    }
 }
