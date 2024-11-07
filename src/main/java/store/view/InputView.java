a7f128fa186ac997ef199c4385b2c1e57dd1fe4c
dbdb456 <adad568@naver.com>
2024-11-07 15:53:57 +0900

    feat(inputview): 멤버십 할인 여부 메서드 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -18,4 +18,9 @@ public class InputView {
         System.out.println("현재 " + productName + " " + number + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
         return Console.readLine();
     }
+
+    public String inputConfirmMembershipDiscount() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
 }
