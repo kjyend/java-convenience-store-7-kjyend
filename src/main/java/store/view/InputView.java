9bb62afb70e8a01183ae1c9b9a56ab1886a182f5
dbdb456 <adad568@naver.com>
2024-11-07 15:42:43 +0900

    feat(inputview): 프로모션 제품 추가 여부 메서드 추가

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -8,4 +8,9 @@ public class InputView {
         System.out.println("구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return Console.readLine();
     }
+
+    public String inputPromotionAddFreeProduct(String productName, int price) {
+        System.out.println("현재 " + productName + "은(는) " + price + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
+        return Console.readLine();
+    }
 }
