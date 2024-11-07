a294d78e9155c936710a1bf032b0adce3db11d9e
dbdb456 <adad568@naver.com>
2024-11-07 16:40:21 +0900

    feat(validators): 제품 형식 검증 메서드 추가

--- src/main/java/store/validator/Validators.java
+++ src/main/java/store/validator/Validators.java
@@ -1,4 +1,11 @@
 package store.validator;
 
 public class Validators {
+
+    public void validateProductFormat(String product) {
+        String regex = "^[가-힣0-9\\[\\],-]+$";
+        if (!product.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        }
+    }
 }
