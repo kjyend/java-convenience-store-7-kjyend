0355aacd4668fea4aa8ab2a8fa51ee6bdeeb6e73
dbdb456 <adad568@naver.com>
2024-11-08 15:58:56 +0900

    feat(validators): 상품 존재 여부 메서드 추가

--- src/main/java/store/validator/Validators.java
+++ src/main/java/store/validator/Validators.java
@@ -1,5 +1,8 @@
 package store.validator;
 
+import java.util.List;
+import store.domain.Product;
+
 public class Validators {
 
     public void validateProductFormat(String product) {
@@ -7,6 +10,15 @@ public class Validators {
         if (!product.matches(regex)) {
             throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
         }
+    }
+
+    public void validateProductExists(List<Product> listProducts, String product) {
+        for (Product listProduct : listProducts) {
+            if (!listProduct.sameName(product)) {
+                throw new IllegalArgumentException("[ERROR] 존재하지 않는 상품입니다. 다시 입력해 주세요.");
+            }
+        }
+
     }
 
     public void validateYesOrNo(String yesNoInput) {
