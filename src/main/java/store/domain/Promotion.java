61ea524a1acb256e9415de3cf03a035b5c5befb8
dbdb456 <adad568@naver.com>
2024-11-09 14:03:30 +0900

    feat(domain): Promotion 클래스 추가

--- src/main/java/store/domain/Promotion.java
+++ src/main/java/store/domain/Promotion.java
@@ -1,4 +1,48 @@
 package store.domain;
 
+import camp.nextstep.edu.missionutils.DateTimes;
+import java.time.LocalDateTime;
+
 public class Promotion {
+
+    private String name;
+    private int buy;
+    private int free;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public Promotion(String name, String buy, String free, String startDate, String endDate) {
+        this.name = name;
+        this.buy = strToInt(buy);
+        this.free = strToInt(free);
+        this.startDate = date(startDate);
+        this.endDate = date(endDate);
+    }
+
+    private LocalDateTime date(String startDate) {
+        return LocalDateTime.parse(startDate);
+    }
+
+    private int strToInt(String buy) {
+        return Integer.parseInt(buy);
+    }
+
+    public boolean promotionDate() {
+        if (this.startDate.isAfter(DateTimes.now()) && this.endDate.isBefore(DateTimes.now())) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean sameName(String name) {
+        return this.name.equals(name);
+    }
+
+    public int getBuy() {
+        return this.buy;
+    }
+
+    public int getFree() {
+        return this.free;
+    }
 }
