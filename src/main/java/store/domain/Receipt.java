549a5eb21ea8a779a764ef06c63bac3bbb577a4b
dbdb456 <adad568@naver.com>
2024-11-10 21:39:37 +0900

    feat(domain): Receipt 클래스 메서드 추가

--- src/main/java/store/domain/Receipt.java
+++ src/main/java/store/domain/Receipt.java
@@ -1,4 +1,47 @@
 package store.domain;
 
 public class Receipt {
+    private String name;
+    private int quantity;
+    private String promotionName;
+    private int promotionQuantity;
+
+    public Receipt(String name, String quantity, String promotionName, String promotionQuantity) {
+        this.name = name;
+        this.quantity = strToInt(quantity);
+        this.promotionName = promotionName;
+        this.promotionQuantity = strToInt(promotionQuantity);
+    }
+
+    private int strToInt(String str) {
+        return Integer.parseInt(str);
+    }
+
+    public void setPromotionName(String productName) {
+        this.promotionName = productName;
+    }
+
+    public void addPromotionQuantity(int promotionQuantity) {
+        this.promotionQuantity += promotionQuantity;
+    }
+
+    public void addQuantity(int quantity) {
+        this.quantity += quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPromotionName() {
+        return promotionName;
+    }
+
+    public int getPromotionQuantity() {
+        return this.promotionQuantity;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
 }
