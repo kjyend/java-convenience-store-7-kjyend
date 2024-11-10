ece904d74b303439e6f3f1d59c01d7601c1e1cff
dbdb456 <adad568@naver.com>
2024-11-10 20:26:56 +0900

    feat(validators): 입력 형식 검증 메서드 추가

--- src/main/java/store/validator/Validators.java
+++ src/main/java/store/validator/Validators.java
@@ -12,6 +12,13 @@ public class Validators {
         }
     }
 
+    public void validateProductInputFormat(String product) {
+        String regex = "\\[(.+?)-(\\d+)]";
+        if (!product.matches(regex)) {
+            throw new IllegalArgumentException("[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.");
+        }
+    }
+
     public void validateProductExists(List<Product> listProducts, String product) {
         for (Product listProduct : listProducts) {
             if (!listProduct.sameName(product)) {
